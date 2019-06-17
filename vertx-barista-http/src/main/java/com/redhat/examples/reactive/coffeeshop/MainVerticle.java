b005d2efe5bed0dc4ad80c87db9b6cccc255246e
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-06-17 13:35:52 -0400

    implemented config file

--- vertx-barista-http/src/main/java/com/redhat/examples/reactive/coffeeshop/MainVerticle.java
+++ vertx-barista-http/src/main/java/com/redhat/examples/reactive/coffeeshop/MainVerticle.java
@@ -2,10 +2,13 @@ package com.redhat.examples.reactive.coffeeshop;
 
 
 import io.reactivex.Maybe;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.http.HttpServer;
 import io.vertx.reactivex.core.http.HttpServerResponse;
@@ -20,22 +23,41 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(final Future<Void> startFuture) {
-    this.createHttpServer()
+    this.loadConfig()
+      .flatMap(this::createHttpServer)
       .doOnError(startFuture::fail)
       .subscribe(v -> startFuture.complete());
   }
 
   private void baristaHandler(RoutingContext routingContext) {
+
     // fail for now
-    routingContext.response().setStatusCode(500);
+    HttpServerResponse response = routingContext.response();
+    response.putHeader("content-type", "text/plain").end("Welcome to the Reactive Coffeeshop, I'm " + name );
   }
+
+  /*
+    Load our configuration file
+   */
+  private Maybe<JsonObject> loadConfig(){
+
+    ConfigStoreOptions fileStore = new ConfigStoreOptions()
+      .setType("file")
+      .setOptional(true)
+      .setConfig(new JsonObject().put("path", "application-conf.json"));
+    ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(fileStore);
+    ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+    return retriever.rxGetConfig().toMaybe();
+
+  }
+
 
   /*
    * Create an HttpServer with the appropriate routes
    */
-  private Maybe<HttpServer> createHttpServer() {
+  private Maybe<HttpServer> createHttpServer(JsonObject jsonConfig) {
 
-    this.name = config().getString("name", "Godzilla");
+    this.name = jsonConfig.getString("name", "Godzilla");
 
     // Create an instance of Router
     Router baseRouter = Router.router(vertx);
@@ -47,7 +69,7 @@ public class MainVerticle extends AbstractVerticle {
     });
 
     // Handle the barista functions
-    baseRouter.post("/barista").handler(this::baristaHandler);
+    baseRouter.get("/barista").handler(this::baristaHandler);
 
     return vertx.createHttpServer()
       .requestHandler(baseRouter::accept).rxListen().toMaybe();
