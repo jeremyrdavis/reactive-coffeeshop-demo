f22cdaa09b5e0338c607df8f6a42597047e72fa2
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-06-18 10:39:39 -0400

    removed http functionality from MainVerticle

---
+++ vertx-barista-http/src/main/java/com/redhat/examples/reactive/coffeeshop/HttpBaristaVerticle.java
@@ -0,0 +1,95 @@
+package com.redhat.examples.reactive.coffeeshop;
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.vertx.core.Future;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerResponse;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class HttpBaristaVerticle extends AbstractVerticle{
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  private String name;
+
+  private Random random = new Random();
+
+  @Override
+  public void start(final Future<Void> startFuture) {
+    this.createHttpServer()
+      .doOnError(startFuture::fail)
+      .subscribe(v -> startFuture.complete());
+  }
+
+  /*
+   * Create an HttpServer with the appropriate routes
+   */
+  private Maybe<HttpServer> createHttpServer() {
+
+    this.name = config().getString("name", "Godzilla");
+
+    // Create an instance of Router
+    Router baseRouter = Router.router(vertx);
+
+    // Handle the root url
+    baseRouter.route("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/plain").end("Welcome to the Reactive Coffeeshop with Eclipse Vert.x!");
+    });
+
+    // Handle the barista functions
+    baseRouter.route("/barista*").handler(BodyHandler.create());
+    baseRouter.get("/barista").handler(this::baristaHandler);
+    baseRouter.post("/barista").handler(this::orderHandler);
+
+    return vertx.createHttpServer()
+      .requestHandler(baseRouter::accept).rxListen().toMaybe();
+  }
+
+  private Observable<Beverage> makeIt(Order order) {
+    return Single.just(new Beverage(order, name)).toObservable();
+  }
+
+  /*
+    Handler for posting an order
+   */
+  private void orderHandler(RoutingContext routingContext) {
+
+    LOG.debug("orderHandler");
+    LOG.debug(routingContext.getBody());
+    System.out.println("body: " + routingContext.getBodyAsString());
+    JsonObject postBody = routingContext.getBodyAsJson();
+
+    Observable.zip(
+      makeIt(new Order(postBody.getString("product"), postBody.getString("name"), postBody.getString("orderId").toString())),
+      Observable.interval(random.nextInt(5) * 1000, TimeUnit.MILLISECONDS),
+      (obs, timer) -> obs).doOnNext(beverage -> {
+        System.out.println(beverage.toString());
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("Content-Type", "application/json").end(Json.encode(beverage));
+      }
+    ).subscribe();
+  }
+
+  /*
+    Handler to display the name of the Barista
+   */
+  private void baristaHandler(RoutingContext routingContext) {
+
+    HttpServerResponse response = routingContext.response();
+    response.putHeader("content-type", "text/plain").end("Welcome to the Reactive Coffeeshop, I'm " + name);
+  }
+
+}
--- vertx-barista-http/src/main/java/com/redhat/examples/reactive/coffeeshop/MainVerticle.java
+++ vertx-barista-http/src/main/java/com/redhat/examples/reactive/coffeeshop/MainVerticle.java
@@ -2,27 +2,18 @@ package com.redhat.examples.reactive.coffeeshop;
 
 
 import io.reactivex.Maybe;
-import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
-import io.vertx.reactivex.core.http.HttpServer;
-import io.vertx.reactivex.core.http.HttpServerResponse;
-import io.vertx.reactivex.ext.web.Router;
-import io.vertx.reactivex.ext.web.RoutingContext;
-import io.vertx.reactivex.ext.web.handler.BodyHandler;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
-import static io.vertx.ext.web.handler.BodyHandler.*;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -35,16 +26,26 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(final Future<Void> startFuture) {
     this.loadConfig()
-      .flatMap(this::createHttpServer)
+      .flatMap(this::configureDeployment)
+      .flatMap(this::deployBarista)
       .doOnError(startFuture::fail)
       .subscribe(v -> startFuture.complete());
+  }
+
+  private Maybe<String> deployBarista(DeploymentOptions deploymentOptions) {
+    return vertx.rxDeployVerticle(HttpBaristaVerticle.class.getName(), deploymentOptions).toMaybe();
+  }
+
+  private Maybe<DeploymentOptions> configureDeployment(JsonObject jsonObject) {
+    DeploymentOptions opts = new DeploymentOptions();
+    opts.setConfig(jsonObject);
+    return Single.just(opts).toMaybe();
   }
 
   /*
       Load our configuration file from a file in the classpath named, 'application-conf.json'
    */
   private Maybe<JsonObject> loadConfig() {
-
     ConfigStoreOptions fileStore = new ConfigStoreOptions()
       .setType("file")
       .setOptional(true)
@@ -55,63 +56,5 @@ public class MainVerticle extends AbstractVerticle {
 
   }
 
-  /*
-   * Create an HttpServer with the appropriate routes
-   */
-  private Maybe<HttpServer> createHttpServer(JsonObject jsonConfig) {
-
-    this.name = jsonConfig.getString("name", "Godzilla");
-
-    // Create an instance of Router
-    Router baseRouter = Router.router(vertx);
-
-    // Handle the root url
-    baseRouter.route("/").handler(routingContext -> {
-      HttpServerResponse response = routingContext.response();
-      response.putHeader("content-type", "text/plain").end("Welcome to the Reactive Coffeeshop with Eclipse Vert.x!");
-    });
-
-    // Handle the barista functions
-    baseRouter.route("/barista*").handler(BodyHandler.create());
-    baseRouter.get("/barista").handler(this::baristaHandler);
-    baseRouter.post("/barista").handler(this::orderHandler);
-
-    return vertx.createHttpServer()
-      .requestHandler(baseRouter::accept).rxListen().toMaybe();
-  }
-
-  private Observable<Beverage> makeIt(Order order) {
-    return Single.just(new Beverage(order, name)).toObservable();
-  }
-
-  /*
-    Handler for posting an order
-   */
-  private void orderHandler(RoutingContext routingContext) {
-
-    LOG.debug("orderHandler");
-    LOG.debug(routingContext.getBody());
-    System.out.println("body: " + routingContext.getBodyAsString());
-    JsonObject postBody = routingContext.getBodyAsJson();
-
-    Observable.zip(
-      makeIt(new Order(postBody.getString("product"), postBody.getString("name"), postBody.getString("orderId").toString())),
-      Observable.interval(random.nextInt(5) * 1000, TimeUnit.MILLISECONDS),
-      (obs, timer) -> obs).doOnNext(beverage -> {
-        System.out.println(beverage.toString());
-        HttpServerResponse response = routingContext.response();
-        response.putHeader("Content-Type", "application/json").end(Json.encode(beverage));
-      }
-    ).subscribe();
-  }
-
-  /*
-    Handler to display the name of the Barista
-   */
-  private void baristaHandler(RoutingContext routingContext) {
-
-    HttpServerResponse response = routingContext.response();
-    response.putHeader("content-type", "text/plain").end("Welcome to the Reactive Coffeeshop, I'm " + name);
-  }
 
 }
