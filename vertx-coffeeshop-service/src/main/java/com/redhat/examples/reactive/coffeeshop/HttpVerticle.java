eb93c10b422949e4ae9b80fb31263c1609f5c6cd
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-06-22 18:13:24 -0400

    pulling values from post

--- vertx-coffeeshop-service/src/main/java/com/redhat/examples/reactive/coffeeshop/HttpVerticle.java
+++ vertx-coffeeshop-service/src/main/java/com/redhat/examples/reactive/coffeeshop/HttpVerticle.java
@@ -2,9 +2,12 @@ package com.redhat.examples.reactive.coffeeshop;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class HttpVerticle extends AbstractVerticle {
 
@@ -15,6 +18,8 @@ public class HttpVerticle extends AbstractVerticle {
     // initialize the router
     Router baseRouter = Router.router(vertx);
     baseRouter.get("/").handler(this::rootHandler);
+    baseRouter.route("/messaging").handler(BodyHandler.create());
+    baseRouter.post("/messaging").handler(this::messagingHandler);
 
     vertx.createHttpServer()
       .requestHandler(baseRouter::accept)
@@ -26,6 +31,14 @@ public class HttpVerticle extends AbstractVerticle {
         }
       });
 
+  }
+
+  private void messagingHandler(RoutingContext routingContext) {
+    MultiMap formAttributes = routingContext.request().formAttributes();
+    String name = formAttributes.get("name");
+    String beverage = formAttributes.get("beverage");
+    HttpServerResponse response = routingContext.response();
+    response.putHeader("content-type", "text/plain").end("name:" + name + " order:" + beverage);
   }
 
   private void rootHandler(RoutingContext routingContext) {
---
+++ vertx-coffeeshop-service/src/test/java/com/redhat/examples/reactive/coffeeshop/HttpVerticleTest.java
@@ -0,0 +1,48 @@
+package com.redhat.examples.reactive.coffeeshop;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.Checkpoint;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(VertxExtension.class)
+public class HttpVerticleTest {
+
+  private Vertx vertx;
+
+  @Test
+  @DisplayName("Test Ordering a Beverage")
+  public void testOrderingABeverage(Vertx vertx, VertxTestContext tc) {
+
+    WebClient webClient = WebClient.create(vertx);
+    Checkpoint deploymentCheckpoint = tc.checkpoint();
+    Checkpoint requestCheckpoint = tc.checkpoint();
+
+    MultiMap form = MultiMap.caseInsensitiveMultiMap();
+    form.set("name", "Buffy");
+    form.set("beverage", "Venti Dark Roast");
+
+    vertx.deployVerticle(new MainVerticle(), tc.succeeding(id -> {
+
+      deploymentCheckpoint.flag();
+
+      webClient.post(8080, "localhost", "/messaging")
+        .sendForm(form, tc.succeeding(resp -> {
+          tc.verify(() -> {
+            assertThat(resp.statusCode()).isEqualTo(200);
+            assertThat(resp.bodyAsString()).contains("Buffy");
+            requestCheckpoint.flag();
+          });
+        }));
+    }));
+
+  }
+}
