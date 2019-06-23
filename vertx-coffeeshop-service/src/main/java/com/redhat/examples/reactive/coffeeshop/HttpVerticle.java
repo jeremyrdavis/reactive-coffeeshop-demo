e314eaecc2863c94e885a335b7497af103944c52
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-06-22 20:44:22 -0400

    sending to both queues

--- vertx-coffeeshop-service/src/main/java/com/redhat/examples/reactive/coffeeshop/HttpVerticle.java
+++ vertx-coffeeshop-service/src/main/java/com/redhat/examples/reactive/coffeeshop/HttpVerticle.java
@@ -4,11 +4,8 @@ import com.redhat.examples.reactive.coffeeshop.model.Order;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
-import io.vertx.core.MultiMap;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -23,6 +20,7 @@ import java.util.UUID;
 public class HttpVerticle extends AbstractVerticle {
 
   private KafkaProducer<String, String> kafkaProducer;
+
   private Future<Void> testKafkaSendFuture;
 
   @Override
@@ -63,6 +61,7 @@ public class HttpVerticle extends AbstractVerticle {
   }
 
   private void messagingHandler(RoutingContext routingContext) {
+
     Order order = new Order(
       routingContext.request().formAttributes().get("beverage"),
       routingContext.request().formAttributes().get("name"));
@@ -70,6 +69,19 @@ public class HttpVerticle extends AbstractVerticle {
 
     System.out.println(Json.encodePrettily(order));
 
+    CompositeFuture.all(
+      sendOrderToKafka(order, KafkaQueue.ORDERS),
+      sendOrderToKafka(order, KafkaQueue.QUEUE)).setHandler(ar -> {
+      if (ar.succeeded()) {
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("content-type", "application/json").end(Json.encodePrettily(order));
+      }else{
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("content-type", "application/json").end(new String("{ \"error\":\"" + ar.cause() + "\"}"));
+      }
+    });
+
+/*
     KafkaProducerRecord<String, String> record = KafkaProducerRecord.create("orders", Json.encodePrettily(order));
 
     kafkaProducer.write(record, ar -> {
@@ -82,11 +94,26 @@ public class HttpVerticle extends AbstractVerticle {
         response.putHeader("content-type", "application/json").end(new String("{ \"error\":\"" + ar.cause() + "\"}"));
       }
     });
+*/
+
   }
 
   private void rootHandler(RoutingContext routingContext) {
     HttpServerResponse response = routingContext.response();
     response.putHeader("content-type", "text/plain").end("Hello Vert.x!");
+  }
+
+  private Future<Void> sendOrderToKafka(Order order, KafkaQueue kafaQueue){
+    Future<Void> sendFuture = Future.future();
+    KafkaProducerRecord<String, String> record = KafkaProducerRecord.create(kafaQueue.name, Json.encodePrettily(order));
+    kafkaProducer.write(record, ar -> {
+      if (ar.failed()) {
+        sendFuture.fail(ar.cause());
+      }else{
+        sendFuture.complete();
+      }
+    });
+    return sendFuture;
   }
 
   private Future<Void> initKafkaProducer() {
@@ -106,6 +133,17 @@ public class HttpVerticle extends AbstractVerticle {
     }
 
     return initKafkaProducerFuture;
+  }
+
+  enum KafkaQueue{
+
+  ORDERS("orders"), QUEUE("queue");
+
+    public String name;
+
+    private KafkaQueue(String name){
+      this.name = name;
+    }
   }
 
 }
